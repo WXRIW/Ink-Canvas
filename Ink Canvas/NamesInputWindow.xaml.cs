52e1151f9a21055880b6d0fe814d2d7419341f0d
XY Wang <wxy11787@foxmail.com>
2021-10-30 23:55:38 +0800

    Add built-in Names Editor

---
+++ Ink Canvas/NamesInputWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ink_Canvas
+{
+    /// <summary>
+    /// Interaction logic for NamesInputWindow.xaml
+    /// </summary>
+    public partial class NamesInputWindow : Window
+    {
+        public NamesInputWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "Names.txt"))
+            {
+                TextBoxNames.Text = File.ReadAllText(System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "Names.txt");
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (File.ReadAllText(System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "Names.txt") != TextBoxNames.Text)
+            {
+                var result = MessageBox.Show("是否保存？", "名单导入", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes)
+                {
+                    File.WriteAllText(System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "Names.txt", TextBoxNames.Text);
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
--- Ink Canvas/RandWindow.xaml.cs
+++ Ink Canvas/RandWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using ModernWpf.Controls;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -27,12 +29,15 @@ namespace Ink_Canvas
         }
 
         public int TotalCount = 1;
+        public int PeopleCount = 60;
         public List<string> Names = new List<string>();
 
         private void BorderBtnAdd_MouseUp(object sender, MouseButtonEventArgs e)
         {
+            if (TotalCount >= PeopleCount) return;
             TotalCount++;
             LabelNumberCount.Content = TotalCount.ToString();
+            SymbolIconStart.Symbol = Symbol.People;
         }
 
         private void BorderBtnMinus_MouseUp(object sender, MouseButtonEventArgs e)
@@ -40,49 +45,142 @@ namespace Ink_Canvas
             if (TotalCount < 2) return;
             TotalCount--;
             LabelNumberCount.Content = TotalCount.ToString();
+            if (TotalCount == 1)
+            {
+                SymbolIconStart.Symbol = Symbol.Contact;
+            }
         }
 
         private void BorderBtnRand_MouseUp(object sender, MouseButtonEventArgs e)
         {
             Random random = new Random();
             string outputString = "";
-            for (int i = 0; i < TotalCount; i++)
+            List<string> outputs = new List<string>();
+            List<int> rands = new List<int>();
+
+            LabelOutput2.Visibility = Visibility.Collapsed;
+            LabelOutput3.Visibility = Visibility.Collapsed;
+            BorderBtnRandCover.Visibility = Visibility.Visible;
+
+            new Thread(new ThreadStart(() =>
             {
-                int maxN = 60;
-                if (Names.Count != 0)
+                for (int i = 0; i < 5; i++)
                 {
-                    maxN = Names.Count;
+                    int rand = random.Next(1, PeopleCount);
+                    while (rands.Contains(rand))
+                    {
+                        rand = random.Next(1, PeopleCount);
+                    }
+                    rands.Add(rand);
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        if (Names.Count != 0)
+                        {
+                            LabelOutput.Content = Names[rand];
+                        }
+                        else
+                        {
+                            LabelOutput.Content = rand.ToString();
+                        }
+                    });
+
+                    Thread.Sleep(150);
                 }
-                int rand = random.Next(1, maxN);
-                if (Names.Count != 0)
+
+                rands = new List<int>();
+                Application.Current.Dispatcher.Invoke(() =>
                 {
-                    outputString += Names[rand] + Environment.NewLine;
-                }
-                else
-                {
-                    outputString += rand.ToString() + Environment.NewLine;
-                }
-            }
-            LabelOutput.Content = outputString.ToString().Trim();
+                    for (int i = 0; i < TotalCount; i++)
+                    {
+                        int rand = random.Next(1, PeopleCount);
+                        while (rands.Contains(rand))
+                        {
+                            rand = random.Next(1, PeopleCount);
+                        }
+                        rands.Add(rand);
+
+                        if (Names.Count != 0)
+                        {
+                            outputs.Add(Names[rand]);
+                            outputString += Names[rand] + Environment.NewLine;
+                        }
+                        else
+                        {
+                            outputs.Add(rand.ToString());
+                            outputString += rand.ToString() + Environment.NewLine;
+                        }
+                    }
+                    if (TotalCount <= 5)
+                    {
+                        LabelOutput.Content = outputString.ToString().Trim();
+                    }
+                    else if (TotalCount <= 10)
+                    {
+                        LabelOutput2.Visibility = Visibility.Visible;
+                        outputString = "";
+                        for (int i = 0; i < (outputs.Count + 1) / 2; i++)
+                        {
+                            outputString += outputs[i].ToString() + Environment.NewLine;
+                        }
+                        LabelOutput.Content = outputString.ToString().Trim();
+                        outputString = "";
+                        for (int i = (outputs.Count + 1) / 2; i < outputs.Count; i++)
+                        {
+                            outputString += outputs[i].ToString() + Environment.NewLine;
+                        }
+                        LabelOutput2.Content = outputString.ToString().Trim();
+                    }
+                    else
+                    {
+                        LabelOutput2.Visibility = Visibility.Visible;
+                        LabelOutput3.Visibility = Visibility.Visible;
+                        outputString = "";
+                        for (int i = 0; i < (outputs.Count + 1) / 3; i++)
+                        {
+                            outputString += outputs[i].ToString() + Environment.NewLine;
+                        }
+                        LabelOutput.Content = outputString.ToString().Trim();
+                        outputString = "";
+                        for (int i = (outputs.Count + 1) / 3; i < (outputs.Count + 1) * 2 / 3; i++)
+                        {
+                            outputString += outputs[i].ToString() + Environment.NewLine;
+                        }
+                        LabelOutput2.Content = outputString.ToString().Trim();
+                        outputString = "";
+                        for (int i = (outputs.Count + 1) * 2 / 3; i < outputs.Count; i++)
+                        {
+                            outputString += outputs[i].ToString() + Environment.NewLine;
+                        }
+                        LabelOutput3.Content = outputString.ToString().Trim();
+                    }
+                    BorderBtnRandCover.Visibility = Visibility.Collapsed;
+                });
+            })).Start();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            if (File.Exists("Names.txt"))
+            Names = new List<string>();
+            if (File.Exists(System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "Names.txt"))
             {
-                string[] fileNames = File.ReadAllLines("Names.txt");
+                string[] fileNames = File.ReadAllLines(System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "Names.txt");
 
                 //Fix emtpy lines
                 foreach (string s in fileNames)
                 {
                     if (s != "") Names.Add(s);
                 }
+
+                PeopleCount = Names.Count();
+                TextBlockPeopleCount.Text = PeopleCount.ToString();
             }
         }
 
         private void BorderBtnHelp_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            MessageBox.Show("如需显示姓名，请在程序目录下新建 Names.txt，并将姓名输入，一行一个。");
+            //MessageBox.Show("如需显示姓名，请在程序目录下新建 Names.txt，并将姓名输入，一行一个。");
+            new NamesInputWindow().ShowDialog();
+            Window_Loaded(this, null);
         }
 
         private void BtnClose_MouseUp(object sender, MouseButtonEventArgs e)
