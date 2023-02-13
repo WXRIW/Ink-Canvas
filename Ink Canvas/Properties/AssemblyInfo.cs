5e2b1aa3dc1db1ed10ad46d4ed74f848fc2f1681
XY Wang <wxy11787@foxmail.com>
2023-02-13 17:43:58 +0800

    Release new version

--- Ink Canvas/Properties/AssemblyInfo.cs
+++ Ink Canvas/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("WXRIW")]
 [assembly: AssemblyProduct("Ink Canvas")]
-[assembly: AssemblyCopyright("Copyright © XY Wang 2022")]
+[assembly: AssemblyCopyright("Copyright © XY Wang 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.5.0.0")]
-[assembly: AssemblyFileVersion("3.5.0.0")]
+[assembly: AssemblyVersion("3.6.0.0")]
+[assembly: AssemblyFileVersion("3.6.0.0")]
