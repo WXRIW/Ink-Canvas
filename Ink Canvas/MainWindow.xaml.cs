09aa1c92d642f3d775cbe59eba2e3998496a66b3
clover_yan <56215525+clover-yan@users.noreply.github.com>
2023-05-24 11:55:48 +0800

    Check BorderMode

--- Ink Canvas/MainWindow.xaml.cs
+++ Ink Canvas/MainWindow.xaml.cs
@@ -6657,7 +6657,8 @@ namespace Ink_Canvas
             if (currentMode == 0)
             {
                 //进入黑板
-                Topmost = false;
+                if (App.StartArgs.Contains("-b"))
+                    Topmost = false;
                 if (BtnPPTSlideShowEnd.Visibility == Visibility.Collapsed)
                 {
                     pointDesktop = new Point(ViewboxFloatingBar.Margin.Left, ViewboxFloatingBar.Margin.Top);
@@ -6688,7 +6689,8 @@ namespace Ink_Canvas
             else
             {
                 //关闭黑板
-                Topmost = true;
+                if (App.StartArgs.Contains("-b"))
+                    Topmost = true;
 
                 if (isInMultiTouchMode) BorderMultiTouchMode_MouseUp(null, null);
 
