b4040060f886b25d3b87271895dcc1126f8da271
ChangSakura <ChangSakura@outlook.com>
2023-12-03 01:21:46 +0800

    [fix] 非预期的橡皮大小；软件启动时部分设置选项未更新值

--- Ink Canvas/MainWindow.xaml.cs
+++ Ink Canvas/MainWindow.xaml.cs
@@ -702,6 +702,14 @@ namespace Ink_Canvas
             {
                 ToggleSwitchEnableTwoFingerZoom.IsOn = false;
             }
+            if (Settings.Gesture.IsEnableTwoFingerTranslate)
+            {
+                ToggleSwitchEnableTwoFingerTranslate.IsOn = true;
+            }
+            else
+            {
+                ToggleSwitchEnableTwoFingerTranslate.IsOn = false;
+            }
             if (Settings.Gesture.IsEnableTwoFingerRotation)
             {
                 ToggleSwitchEnableTwoFingerRotation.IsOn = true;
@@ -761,6 +769,8 @@ namespace Ink_Canvas
                 ComboBoxPenStyle.SelectedIndex = Settings.Canvas.InkStyle;
 
                 ComboBoxEraserSize.SelectedIndex = Settings.Canvas.EraserSize;
+
+                ComboBoxHyperbolaAsymptoteOption.SelectedIndex = (int)Settings.Canvas.HyperbolaAsymptoteOption;
             }
             else
             {
@@ -1045,9 +1055,9 @@ namespace Ink_Canvas
                     forcePointEraser = false;
                     break;
             }
+            inkCanvas.EraserShape = forcePointEraser ? new EllipseStylusShape(50, 50) : new EllipseStylusShape(5, 5);
             inkCanvas.EditingMode =
                 forcePointEraser ? InkCanvasEditingMode.EraseByPoint : InkCanvasEditingMode.EraseByStroke;
-            inkCanvas.EraserShape = forcePointEraser ? new EllipseStylusShape(50, 50) : new EllipseStylusShape(5, 5);
             drawingShapeMode = 0;
             GeometryDrawingEraser.Brush = forcePointEraser
                 ? new SolidColorBrush(Color.FromRgb(0x23, 0xA9, 0xF2))
